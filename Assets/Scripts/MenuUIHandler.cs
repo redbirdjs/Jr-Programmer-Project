9e491a66b3b67be45cf9db8a0fac93c2268b93dc
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2024-03-01 09:33:53 +0100

    feat: added unity editor features

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -28,6 +31,10 @@ public class MenuUIHandler : MonoBehaviour
     }
 
     public void Exit() {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
         Application.Quit();
+#endif
     }
 }
