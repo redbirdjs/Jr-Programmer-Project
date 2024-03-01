f00575712f51a7b5e1b7bf7da1bb7fc35d7f5a9b
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2024-03-01 09:47:05 +0100

    feat: Added MainManager to Menu

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static MainManager Instance;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 }
