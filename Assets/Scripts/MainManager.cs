9e6c4ecc3bfeac95dcf29b8aa8859b733cde7349
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2024-03-01 10:02:58 +0100

    feat: LoadColor method added

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -17,6 +17,7 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadColor();
     }
 
     [System.Serializable]
@@ -33,5 +34,15 @@ public class MainManager : MonoBehaviour
         string json = JsonUtility.ToJson(data);
 
         File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor() {
+        string path = Application.persistentDataPath + "savefile.json";
+        if (File.Exists(path)) {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
     }
 }
